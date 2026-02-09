51b8097698ce006a54a832e68a8897e76feb83c8
Matilde José Silva Oliveira <161059956+a103881@users.noreply.github.com>
2026-02-09 00:20:12 +0000

    hello world, vamonos

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/matildez0/simple-go-mod/config"
 	"github.com/matildez0/simple-go-mod/models"
 )
@@ -17,10 +18,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	router := mux.NewRouter()
+	router.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, World!"))
+	}).Methods("GET")
 
 	defer dbConnection.Close()
 
 	//incializa o servidor e as rotas
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
